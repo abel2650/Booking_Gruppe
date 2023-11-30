75fb3a494ecbf711dc60b6d08d5da0d1e8d012b1
RebinIsmail <rei001@edu.zealand.dk>
2023-11-30 13:12:48 +0100

    newest

---
+++ Booking_Gruppe/Pages/Bookings/Booking.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Booking_Gruppe.Bookings;
+
+public class BookingModel : PageModel
+{
+    private PriserRepository _priserRepository;
+
+    public PriserRepository PriserRepository
+    {
+        get { return _priserRepository; }
+        set { _priserRepository = value; }
+    }
+
+    public BookingModel(PriserRepository priser)
+    {
+        _priserRepository = priser;
+    }
+
+    public void OnGet()
+    {
+        // Implementer logik for HTTP GET-anmodningen her
+    }
+}
