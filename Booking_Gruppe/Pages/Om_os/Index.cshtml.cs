2909f9c44a5a2fd03b47a614c849c099a2bc2dd4
Marcus Nørregaard Hartmann <mah016@edu.zealand.dk>
2023-12-01 11:31:26 +0100

    Om os

---
+++ Booking_Gruppe/Pages/Om_os/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Booking_Gruppe.Pages.Om_os
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
