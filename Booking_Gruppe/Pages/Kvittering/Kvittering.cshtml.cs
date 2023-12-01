8d6e07c9f749cc9cedb7dfef7b604328a7eff60c
Marcus Nørregaard Hartmann <mah016@edu.zealand.dk>
2023-12-01 12:57:22 +0100

    Kvittering

---
+++ Booking_Gruppe/Pages/Kvittering/Kvittering.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Booking_Gruppe.Pages
+{
+    public class KvitteringModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
