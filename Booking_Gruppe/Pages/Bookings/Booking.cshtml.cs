80485aec51f719257ef5500ea1e19795e3ab6022
abel2650 <149366714+abel2650@users.noreply.github.com>
2023-12-06 16:12:37 +0100

    tilføjet flere

--- Booking_Gruppe/Pages/Bookings/Booking.cshtml.cs
+++ Booking_Gruppe/Pages/Bookings/Booking.cshtml.cs
@@ -27,8 +27,16 @@ public class Booking : PageModel
         _listFrisør = repo.ListeAfFrisør ?? new List<Frisør>();
         if (_listFrisør.Count == 0)
         {
-            _listFrisør.Add(new Frisør(1, "Herreklip", "Frisør", 75.2, true));
-            _listFrisør.Add(new Frisør(2, "Dame klip", "Frisør", 75.2, true));
+            _listFrisør.Add(new Frisør(1, "Herreklip", "45 min", 300, true));
+            _listFrisør.Add(new Frisør(2, "Dame klip", "1 time", 1000, true));
+            _listFrisør.Add(new Frisør(6, "Dame permanent", "1,30 time", 500, true));
+            _listFrisør.Add(new Frisør(8, "Makeup", "30 time", 250, true));
+            _listFrisør.Add(new Frisør(9, "Hårstyling", "50 min", 350, true));
+            _listFrisør.Add(new Frisør(10, "Neglelak", "20", 200, true));
+            _listFrisør.Add(new Frisør(11, "Brudeopsætning", "3 time", 2000, true));
+            _listFrisør.Add(new Frisør(12, "Skægtrimning", "25 min", 150, true));
+            _listFrisør.Add(new Frisør(13, "Børneklip", "40 min", 150, true));
+
         }
     }
             
